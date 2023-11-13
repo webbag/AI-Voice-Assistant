6dceb74c35b191588a77ad31b024ac470d318a68
kris <k.kromolicki@gmail.com>
2023-11-13 23:03:02 +0100

    button add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.devkro.aiva"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.devkro.aiva"
@@ -33,7 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
--- app/src/main/java/com/devkro/aiva/MainActivity.kt
+++ app/src/main/java/com/devkro/aiva/MainActivity.kt
@@ -2,10 +2,21 @@ package com.devkro.aiva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devkro.aiva.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var click = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.kkButton.setOnClickListener(){
+            click += 1
+            binding.kkText.text = click.toString()
+        }
     }
 }
